e75d68a6fa32a6de357bd3040d57de0a16bab446
Bertrand Lesne <enselb@gmail.com>
2025-12-08 21:56:40 +0100

    fonctions de visualisation avancées + maj lightGBM + feature eng

---
+++ forecast_2000/utils/feature_engineering.py
@@ -0,0 +1,63 @@
+
+from tqdm import tqdm
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning, module="tqdm")
+
+def features_engineering(df):
+    """Crée de nouvelles caractéristiques temporelles pour un DataFrame de ventes.
+
+    Cette fonction enrichit le DataFrame en ajoutant plusieurs types de
+    caractéristiques basées sur l'historique des ventes ('sales'), groupées par
+    produit ('item_id') et magasin ('store_id').
+
+    Les caractéristiques créées sont :
+    - Lags : Valeurs des ventes des jours précédents (J-28, J-35, etc.).
+    - Rolling Mean : Moyenne mobile des ventes sur différentes fenêtres de temps.
+    - Expanding Mean : Moyenne cumulative des ventes depuis le début de la série.
+
+    Les valeurs NaN générées par ces opérations sont remplacées par 0.
+
+    Args:
+        df (pd.DataFrame): DataFrame contenant les données de ventes. Doit inclure
+                           les colonnes 'item_id', 'store_id', et 'sales'.
+
+    Returns:
+        pd.DataFrame: Le DataFrame d'entrée enrichi avec les nouvelles
+                      caractéristiques.
+    """
+    # Lags
+    lags = [28, 35, 42, 49, 56, 63, 70]
+    for lag in tqdm(lags, desc="Création des Lags"):
+        col_name = f'lag_{lag}'
+        df[col_name] = df.groupby(['item_id', 'store_id'])['sales'].shift(lag)
+        df[col_name] = df[col_name].fillna(0)
+
+    # Rolling Window
+    windows = [28]
+    for window in tqdm(windows, desc="Création des Rolling Means"):
+        col_name = f'rolling_mean_{window}'
+        df[col_name] = df.groupby(['item_id', 'store_id'])['sales'].transform(lambda s: s.rolling(window).mean())
+        df[col_name] = df[col_name].fillna(0)
+
+    # Expanding Window
+    print("Création de l'Expanding Mean...")
+    df['expanding_mean'] = df.groupby(['item_id', 'store_id'])['sales'].transform(lambda s: s.expanding().mean())
+    df['expanding_mean'] = df['expanding_mean'].fillna(0)
+
+    # Calcul du prix moyen historique par article
+    df['item_avg_price'] = df.groupby('item_id')['sell_price'].transform('mean')
+
+    # Création du Ratio : Prix actuel / Prix moyen
+    # Si > 1 : Le produit est plus cher que d'habitude.
+    # Si < 1 : Le produit est en "promo".
+    df['price_ratio'] = df['sell_price'] / df['item_avg_price']
+
+    # Nettoyage
+    df['price_ratio'] = df['price_ratio'].fillna(1.0)
+
+
+
+    print("Feature engineering terminé.")
+
+    return df
